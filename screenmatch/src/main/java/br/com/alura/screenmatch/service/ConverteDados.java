6fe465c031e10c35ca9f71b54b8b0c72b343d772
rmmar <rmmar@ZEN>
2025-05-07 18:42:48 -0300

    Removendo maps

--- screenmatch/src/main/java/br/com/alura/screenmatch/service/ConverteDados.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/service/ConverteDados.java
@@ -1,13 +1,11 @@
 package br.com.alura.screenmatch.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ConverteDados implements IConverteDados {
 	
 	private ObjectMapper mapper = new ObjectMapper();
-	private ObjectMapper maps = new ObjectMapper();
 
 
 	@Override
